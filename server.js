e6b57e837a39aa6368c3b1447a6d11e7a89314c3
Joshua Small <jmsmall89@gmail.com>
2020-10-14 11:16:16 -0400

    REMOVED PATHS
    Server.js paths not working. Removed to start over.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const mongojs = require("mongojs");
 const logger = require("morgan");
-const path = require("path");
+const mongoose = require("mongoose");
+const db = require("./models");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  "mongodb://localhost/workout",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,101 +21,6 @@ mongoose.connect(
   },
   (err) => console.log(err)
 );
-
-const collections = ["plans"];
-const db = mongojs(MONGODB_URI, collections);
-
-db.on("error", (error) => {
-  console.log("Database Error:", error);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
-});
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-
-  db.workouts.insert(req.body, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.get("/all", (req, res) => {
-  db.workouts.find({}, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
-app.get("/find/:id", (req, res) => {
-  db.workouts.findOne(
-    {
-      _id: mongojs.ObjectId(req.params.id),
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-app.post("/update/:id", (req, res) => {
-  db.workouts.update(
-    {
-      _id: mongojs.ObjectId(req.params.id),
-    },
-    {
-      $set: {
-        title: req.body.title,
-        workout: req.body.workout,
-        modified: Date.now(),
-      },
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-app.delete("/delete/:id", (req, res) => {
-  db.workouts.remove(
-    {
-      _id: mongojs.ObjectID(req.params.id),
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-app.delete("/clearall", (req, res) => {
-  db.workouts.remove({}, (error, response) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(response);
-    }
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
