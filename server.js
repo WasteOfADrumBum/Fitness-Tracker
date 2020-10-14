5c79716550970be9a3f27af7c5142b655c063e67
Joshua Small <jmsmall89@gmail.com>
2020-10-14 10:48:13 -0400

    UPDATE MONGO/HEROKU/ATLAS SERVER

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 mongoose.connect(
-  "mongodb://localhost/workout",
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   },
   (err) => console.log(err)
 );
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
