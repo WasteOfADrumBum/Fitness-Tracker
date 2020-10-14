2e67dfdd689bb0977441dfb7c6b6aa53bf1e7710
Joshua Small <jmsmall89@gmail.com>
2020-10-14 10:55:24 -0400

    SERVER.JS PATH UPDATES

--- server.js
+++ server.js
@@ -1,25 +1,112 @@
 const express = require("express");
+const mongojs = require("mongojs");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-  (err) => console.log(err)
-);
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+const collections = ["workouts"];
+
+const db = mongojs(MONGODB_URI, collections);
+
+db.on("error", (error) => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
+  db.workouts.insert(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get("/all", (req, res) => {
+  db.workouts.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get("/find/:id", (req, res) => {
+  db.workouts.findOne(
+    {
+      _id: mongojs.ObjectId(req.params.id),
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.post("/update/:id", (req, res) => {
+  db.workouts.update(
+    {
+      _id: mongojs.ObjectId(req.params.id),
+    },
+    {
+      $set: {
+        title: req.body.title,
+        workout: req.body.workout,
+        modified: Date.now(),
+      },
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  db.workouts.remove(
+    {
+      _id: mongojs.ObjectID(req.params.id),
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.delete("/clearall", (req, res) => {
+  db.workouts.remove({}, (error, response) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(response);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
