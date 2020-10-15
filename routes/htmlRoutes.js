c3b003ac327ea3ee54172e6b89b247dc82540711
Joshua Small <jmsmall89@gmail.com>
2020-10-15 18:40:12 -0400

    ADDED ROUTES & FORMATTING

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = (app) => {
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./models");
 
+// Express Setup
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Morgan Logger
 app.use(logger("dev"));
+
+// Express data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -22,6 +27,11 @@ mongoose.connect(
   (err) => console.log(err)
 );
 
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Server Port
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
